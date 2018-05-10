7d9b3c70edc1debd04b3a6a61b62695156850190
Z. Zhang <tonovate@gmail.com>
2018-05-11 00:22:09 +0800

    csv细节调整/增加预测功能
    1. csv细节调整：读取和保存时，标题都使用英文，避免pandas无法读取标题栏/Excel读取时乱码
    2. 添加一年后预测功能

--- GPA_Programming.py
+++ GPA_Programming.py
@@ -11,7 +11,7 @@ def calGPA(GPA, totalCredit, totalPoint, totalHundredPoint):
     goalGPA = []
     result = []
     hundredResult = []
-    boundary = 4.91
+    boundary = 4.75
     for i in np.arange(GPA, boundary, 0.01):
         x = (totalCredit * i - totalPoint) / (5 - i)
         goalGPA.append(round(i, 2))
@@ -19,29 +19,33 @@ def calGPA(GPA, totalCredit, totalPoint, totalHundredPoint):
         hundredPoint = round(((totalHundredPoint + x * 95) / (totalCredit + x)), 1)
         hundredResult.append(hundredPoint)
 
-    rawData = {'目标GPA': goalGPA, '需要的优秀学分': result, '百分制': hundredResult}
-    dfResult = pd.DataFrame(rawData, columns=['目标GPA', '需要的优秀学分', '百分制'])
-    dfResult.to_csv('goalGPA.csv')
+    rawData = {'Goal GPA': goalGPA, '"A" credits needed': result, 'GPA in 100': hundredResult}
+    dfResult = pd.DataFrame(rawData, columns=['Goal GPA', '"A" credits needed', 'GPA in 100'])
+    dfResult.to_csv('goal_GPA.csv')
 
     plt.plot(goalGPA, result)
-    print('{}:\t{}\t{}'.format('目标GPA', '需要的优秀学分', '百分制'))
-    for i in zip(goalGPA, result, hundredResult):
-        print('{}:\t{}\t{}'.format(i[0], i[1], i[2]))
+    # print('{}:\t{}\t{}'.format('目标GPA', '需要的优秀学分', '百分制'))
+    # for i in zip(goalGPA, result, hundredResult):
+        # print('{}:\t{}\t{}'.format(i[0], i[1], i[2]))
+    best_gpa_in_one_year = (totalPoint + 20 * 2 * 5) / (totalCredit + 40)
+    best_gpa_in_100 = (totalPoint + 20 * 2 * 5) / (totalCredit + 40) * 10 + 45
+    print('若每学期获得20学分的优，预计一年后最好绩点是%.2f(百分制%.2f)。' % (best_gpa_in_one_year, best_gpa_in_100))
+
     plt.show()
 
 
 def main():
-    file = pd.read_csv('GPA.csv')
+    file = pd.read_csv('raw_GPA.csv')
     df = pd.DataFrame(file)
 
     totalCredit = float(df.sum(axis=0)[0])
-    print('总学分=', totalCredit)
-    totalPoint = df['学分'].mul(df['绩点']).sum(axis=0)
+    print('当前总学分 =', totalCredit)
+    totalPoint = df['credit'].mul(df['score']).sum(axis=0)
     GPA = round(totalPoint / totalCredit, 2)
-    print('总绩点=', GPA)
+    print('当前总绩点 =', GPA)
 
     hundred = []
-    for i in df['绩点']:
+    for i in df['score']:
         if i == 5:
             hundred.append(95)
         elif i == 4:
@@ -52,9 +56,9 @@ def main():
             hundred.append(65)
         else:
             hundred.append(30)
-    totalHundredPoint = df['学分'].mul(hundred).sum(axis=0)
+    totalHundredPoint = df['credit'].mul(hundred).sum(axis=0)
     hundredPoint = round(totalHundredPoint / totalCredit, 1)
-    print('百分制=', hundredPoint)
+    print('当前百分制学分 =', hundredPoint)
 
     calGPA(GPA, totalCredit, totalPoint, totalHundredPoint)
 
